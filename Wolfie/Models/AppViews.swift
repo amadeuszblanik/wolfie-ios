4585f8c440ad21ab411991f527cd64e351b18292
Amadeusz Blanik <amadeusz@blanik.me>
2022-11-10 23:35:12 +0100

    Removed weight single view

--- Wolfie/Models/AppViews.swift
+++ Wolfie/Models/AppViews.swift
@@ -42,7 +42,6 @@ enum GuestViews: String, CaseIterable {
 enum DashboardViews: String, CaseIterable {
     case details = "details"
     case weight = "weight"
-    case weightSingle = "weight_single"
     case healthLog = "health_log"
     case healthLogSingle = "health_log_single"
 }
