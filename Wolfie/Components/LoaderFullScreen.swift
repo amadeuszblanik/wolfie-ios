0b1efd444953bc2e7eff95aa1e4839022f4f4fbd
Amadeusz Blanik <amadeusz@blanik.me>
2022-11-14 23:51:56 +0100

    Added weight delete view

---
+++ Wolfie/Components/LoaderFullScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LoaderFullScreen.swift
+//  Wolfie
+//
+//  Created by Amadeusz Blanik on 14/11/2022.
+//
+
+import SwiftUI
+
+struct LoaderFullScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoaderFullScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LoaderFullScreen()
+    }
+}
