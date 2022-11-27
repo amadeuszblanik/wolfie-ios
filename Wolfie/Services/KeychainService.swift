3a11095b0780cba0a804903c9368413a4736e33e
Amadeusz Blanik <amadeusz@blanik.me>
2022-11-27 20:58:55 +0100

    Added Splash Screen

--- Wolfie/Services/KeychainService.swift
+++ Wolfie/Services/KeychainService.swift
@@ -14,36 +14,42 @@ enum KeychainError: Error {
 final class KeychainService {
     static let standard = KeychainService()
 
-    private init() {}
-    
+    private init() { }
+
     func save(_ data: Data, service: String, account: String) throws {
         let isExists = self.read(service: service, account: account)
-        
+
         let query = [
             kSecValueData: data,
             kSecClass: kSecClassGenericPassword,
             kSecAttrService: service,
             kSecAttrAccount: account,
         ] as CFDictionary
-        
+
         let queryUpdate = [
             kSecAttrService: service,
             kSecAttrAccount: account,
             kSecClass: kSecClassGenericPassword,
         ] as CFDictionary
-        
+
         let attributesToUpdate = [kSecValueData: data] as CFDictionary
 
         let status = isExists == nil ? SecItemAdd(query, nil) : SecItemUpdate(queryUpdate as CFDictionary, attributesToUpdate)
-        
+
         if status != errSecSuccess {
-            print("🔑 Error saving: \(status)")
+            let message = """
+            🔑 Keychain Error
+            🔑 Status: \(status)
+            🔑 Description: \(status.description)
+            🔑 Is exists: \(isExists == nil  ? "true" : "false")
+            """
+            print(message)
             sentryLog("Cannot save data to Keychain \(status)")
-            
+
             throw KeychainError.cannotSave(status)
         }
     }
-    
+
     func read(service: String, account: String) -> Data? {
         let query = [
             kSecReturnData: true,
@@ -51,20 +57,20 @@ final class KeychainService {
             kSecAttrService: service,
             kSecAttrAccount: account,
         ] as CFDictionary
-        
+
         var result: AnyObject?
         SecItemCopyMatching(query, &result)
-        
+
         return (result as? Data)
     }
-    
+
     func delete(service: String, account: String) -> Void {
         let query = [
             kSecClass: kSecClassGenericPassword,
             kSecAttrService: service,
             kSecAttrAccount: account,
         ] as CFDictionary
-        
+
         SecItemDelete(query)
     }
 }
