a0295144b1efdd6fdd37280760cfb19faac1ee7f
Amadeusz Blanik <amadeusz@blanik.me>
2022-11-30 04:56:19 +0100

    Added Offline mode detection

--- Wolfie/Services/NetworkService.swift
+++ Wolfie/Services/NetworkService.swift
@@ -11,16 +11,15 @@ import Network
 final class NetworkService: ObservableObject {
     let monitor = NWPathMonitor()
     let queue = DispatchQueue(label: "NetworkSerivce")
-    
+
     @Published var isConnected = true
-    
+
     init() {
         monitor.pathUpdateHandler = { [weak self] path in
             DispatchQueue.main.async {
                 self?.isConnected = path.status == .satisfied ? true : false
             }
         }
-        
-        monitor.start(queue: queue)
+        monitor.start(queue: q)
     }
 }
